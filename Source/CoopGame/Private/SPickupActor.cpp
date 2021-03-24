ad20b166f309747195254b12d2869889446c7281
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-03-25 00:45:27 +0300

    started Powerups; which will be an actor that will spawn another actor or give certain abilities to player; create SPickup and added SphereComp and a decalcomp

---
+++ Source/CoopGame/Private/SPickupActor.cpp
@@ -0,0 +1,37 @@
+// by Salim Pamukcu, 2021
+
+
+#include "SPickupActor.h"
+#include "Components/SphereComponent.h"
+#include "Components/DecalComponent.h"
+
+
+ASPickupActor::ASPickupActor()
+{
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetSphereRadius(75.0f);
+	RootComponent = SphereComp;
+
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
+	DecalComp->SetRelativeRotation(FRotator(90.0f, 0.0f, 0.0f));
+	DecalComp->DecalSize = FVector(64.0f, 75.0f, 75.0f);
+	DecalComp->SetupAttachment(RootComponent);
+
+}
+
+
+void ASPickupActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASPickupActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	// TODO: give a powerup to player and/or spawn a powerup actor
+}
+
+
+
